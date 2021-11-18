4641e06cb780c0eebab38607098aac40fb04cd54
Chillwave <JGreene3387@gmail.com>
2021-11-17 21:40:20 -0700

    added scoreboard

--- rps.js
+++ rps.js
@@ -1,36 +1,65 @@
 const container = document.querySelector("#container");
 const content = document.createElement("div");
 const title = document.createElement("p");
-const butConatainer = document.createElement("div");
-
+const butContainer = document.createElement("div");
+const playerScore = document.createElement("div");
+const playerName = document.createElement("p");
+const compName = document.createElement("p");
+const compScore = document.createElement("div");
+const scoreBoard =  document.createElement("div");
+const playerSide = document.createElement("div");
+const compSide = document.createElement("div");
 
 const rockB = document.createElement('button');
 const paperB = document.createElement('button');
 const scissorsB = document.createElement('button');
 
-butConatainer.classList.add("butContainer");
+butContainer.classList.add("butContainer");
 title.classList.add("title");
 content.classList.add("content");
 rockB.classList.add("cButton");
 paperB.classList.add("cButton");
 scissorsB.classList.add("cButton");
+scoreBoard.classList.add("scoreBoard");
+playerSide.classList.add("playerSide");
+compSide.classList.add("compSide");
+playerName.classList.add("bigName");
+compName.classList.add("bigName");
+playerScore.classList.add("bigScore");
+compScore.classList.add("bigScore");
 
 title.textContent = "ROCK PAPER SCISSORS";
 rockB.textContent = "ROCK";
 paperB.textContent = "PAPER";
 scissorsB.textContent = "SCISSORS";
+playerName.textContent = "PLAYER";
+compName.textContent = "COMPUTER";
+playerScore.textContent = "0";
+compScore.textContent = "0";
+
+// playerScore.textContent = `${pScore}`
+// compScore.textContent = `${cScore}`
 
 container.appendChild(content);
 content.appendChild(title);
-content.appendChild(butConatainer);
-butConatainer.appendChild(rockB);
-butConatainer.appendChild(paperB);
-butConatainer.appendChild(scissorsB);
+content.appendChild(butContainer);
+content.appendChild(scoreBoard);
+butContainer.appendChild(rockB);
+butContainer.appendChild(paperB);
+butContainer.appendChild(scissorsB);
+scoreBoard.appendChild(playerSide);
+scoreBoard.appendChild(compSide);
+playerSide.appendChild(playerName);
+playerSide.appendChild(playerScore);
+compSide.appendChild(compName);
+compSide.appendChild(compScore);
 
 function game() { 
     let computerChoice = computerPlay()
     let playerChoice = prompt("Choose rock, paper or scissors").toLowerCase().trim()
     let outcome = ""
+    let pScore = 0
+    let cScore = 0
 
     function computerPlay() {
         return Math.floor(Math.random() * 3)+ 1
@@ -39,23 +68,26 @@ function game() {
     function playRound (computerChoice, playerChoice) {
     
     if (playerChoice === "rock") {
-        playerChoice = 1
+        playerChoice = "rock"
         console.log("you chose rock")
     } else if (playerChoice === "paper") {
-        playerChoice = 2
+        playerChoice = "paper"
         console.log("you chose paper")
     } else if (playerChoice === "scissors") {
-        playerChoice = 3
+        playerChoice = "scissors"
         console.log("you chose scissors")
     } else {
         console.log("not a valid choice")
     }
     
     if (computerChoice == 1) {
+        computerChoice = "rock"
         console.log("computer chose rock")
     } else if (computerChoice == 2) {
+        computerChoice = "paper"
         console.log("computer chose paper")
     } else {
+        computerChoice = "scissors"
         console.log("computer chose scissors")
     }
     
@@ -64,17 +96,17 @@ function game() {
         outcome = "you tied"
     } 
     if (
-        playerChoice === 1 && computerChoice === 2 ||
-        playerChoice === 2 && computerChoice === 3 ||
-        playerChoice === 3 && computerChoice === 2
+        playerChoice === "rock" && computerChoice === "paper" ||
+        playerChoice === "paper" && computerChoice === "scissors" ||
+        playerChoice === "scissors" && computerChoice === "paper"
         )
-        { outcome = "you lose"} 
+        { outcome = "you lose"; cScore++} 
     if (
-        playerChoice === 1 && computerChoice === 3 ||
-        playerChoice === 2 && computerChoice === 1 ||
-        playerChoice === 3 && computerChoice === 2
+        playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "paper" && computerChoice === "rock" ||
+        playerChoice === "scissors" && computerChoice === "paper"
     ) 
-    {outcome = "you win"}
+    {outcome = "you win"; pScore++}
 return outcome  
 
 }
